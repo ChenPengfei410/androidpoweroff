95cc36a96a115d535248db496ce9ca1caff99e90
AdamChen <mnre2003@yahoo.com.tw>
2015-03-30 11:29:20 +0800

    add set system time sameple function

--- src/com/adam/app/androidshutdown/MainActivity.java
+++ src/com/adam/app/androidshutdown/MainActivity.java
@@ -2,11 +2,13 @@ package com.adam.app.androidshutdown;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Calendar;
 
 import android.R.string;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
@@ -20,6 +22,7 @@ public class MainActivity extends Activity implements OnClickListener{
 	
 	private Button btn_power_off = null;
 	private Button btn_reboot = null;
+	private Button btn_set_system_time = null;
 	
 	private String sClassName = "android.os.SystemProperties";
 	
@@ -51,6 +54,32 @@ public class MainActivity extends Activity implements OnClickListener{
 //        }
 	}
 	
+	void setDate(Context context, int year, int month, int day) {
+	        Calendar c = Calendar.getInstance();
+
+	        c.set(Calendar.YEAR, year);
+	        c.set(Calendar.MONTH, month);
+	        c.set(Calendar.DAY_OF_MONTH, day);
+	        long when = c.getTimeInMillis();
+
+	        if (when / 1000 < Integer.MAX_VALUE) {
+	            ((AlarmManager) context.getSystemService(Context.ALARM_SERVICE)).setTime(when);
+	        }
+	    }
+	
+	void setTime(Context context, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        long when = c.getTimeInMillis();
+
+        if (when / 1000 < Integer.MAX_VALUE) {
+            ((AlarmManager) context.getSystemService(Context.ALARM_SERVICE)).setTime(when);
+        }
+    }
 	
 	private void setSystemProperty(String key, String str) {
 		
@@ -95,6 +124,8 @@ public class MainActivity extends Activity implements OnClickListener{
 		btn_power_off.setOnClickListener(this);
 		btn_reboot = (Button)this.findViewById(R.id.btn_reboot);
 		btn_reboot.setOnClickListener(this);
+		btn_set_system_time = (Button)this.findViewById(R.id.btn_set_system_time);
+		btn_set_system_time.setOnClickListener(this);
 		
 		mPm = (PowerManager)getSystemService(
                 Context.POWER_SERVICE);
@@ -105,11 +136,14 @@ public class MainActivity extends Activity implements OnClickListener{
 		// TODO Auto-generated method stub
 		switch(arg0.getId()) {
 		case R.id.btn_power_off:
-			PowerOff();
-//			ShutDown();
+//			PowerOff();
+			ShutDown();
 			break;
 		case R.id.btn_reboot:			
 			Reboot();
+			break;
+		case R.id.btn_set_system_time:
+			setTime(this, 9, 00);
 			break;
 		default:
 			break;
